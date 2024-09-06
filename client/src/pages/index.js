de1cd4c3ae0ab7cc47a4ee936ba6ad2477a1a7ca
Pritam Kundu <75939390+warmachine028@users.noreply.github.com>
2024-09-07 03:47:55 +0530

    Update index.js

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,4 +1,4 @@
 export { default as Home } from './Home'
 export { default as Todos } from './Todos'
-export { default as Login } from './Login'
+export { default as LogIn } from './LogIn'
 export { default as NotFound } from './NotFound'
